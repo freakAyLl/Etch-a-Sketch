fde26ca832f1c1dfc8cbe6bb4cba8031c8bfe14e
FreakAyLL <alitofigueras5@gmail.com>
2021-03-10 22:13:23 -0300

    Random color generator on event created

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-const body=document.querySelector('#gridBody');
+const body=document.querySelector('#gridContainer');
 for(let i =0; i<256;i++){
     let div = document.createElement('div')
-    div.style.cssText = 'background-color: white; height: 38px; width: 38px; border: black 1px solid; display: inline-block;'
+    div.style.cssText = 'height: 38px; width: 38px; border: black 1px solid; display: inline-block;'
+    div.classList.add(`1`)
     body.appendChild(div)
-    console.log(div)
+    div.addEventListener('mouseleave',()=>{
+        console.log("cliking")
+        div.style.backgroundColor= '#'+Math.floor(Math.random()*16777215).toString(16);
+        console.log(div.style.backgroundColor)
+    })
 }
 
+function createGrid(){
+    div.style.backgroundColor=white;
+}
